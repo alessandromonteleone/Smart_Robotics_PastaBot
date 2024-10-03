46e289b46e46cb0f7818abbc91d1e2493b7e511b
271539@studenti.unimore.it <271539@studenti.unimore.it>
2024-10-03 16:57:06 +0200

    Completing Image Saver Signed-off by: Luca Inghilterra Email: 271539@studenti.unimore.it

--- src/pastabot_pkg/scripts/image_saver.py
+++ src/pastabot_pkg/scripts/image_saver.py
@@ -8,57 +8,35 @@ import time
 
 class ImageSaver:
     def __init__(self):
-        # Inizializza il nodo ROS
-        rospy.init_node('image_saver', anonymous=True)
-        
-        # Sottoscriviti al topic della camera
+        rospy.init_node('image_saver', anonymous=True)                                              # ROS node initialization
         self.image_sub = rospy.Subscriber('/camera/image_raw', Image, self.image_callback)
-        
-        # Istanzia cv_bridge per convertire tra immagini ROS e OpenCV
-        self.bridge = CvBridge()
-        
-        # Tempo dell'ultima immagine salvata
-        self.last_saved_time = time.time()
-        
-        # Directory in cui salvare le immagini
+        self.bridge = CvBridge()                                                                    # CV Bridge for omages conversion between ROS anc CV images
         self.save_directory = "/home/luca/Scrivania/Smart-Robotics/Progetto/Smart_Robotics_PastaBot/src/pastabot_pkg/images"
-        
-        # Contatore per numerare le immagini
-        self.image_count = 0
+        self.image_count = 0                                                                        # Images counter
+        self.last_saved_time = time.time()                                                          # Time inizialization
         
     def image_callback(self, msg):
-        # Ottieni l'ora corrente
-        current_time = time.time()
-
-        # Salva un'immagine solo se sono passati almeno 5 secondi
+        current_time = time.time()  # Current time
         if current_time - self.last_saved_time >= 5:
             try:
-                # Converti il messaggio ROS in un'immagine OpenCV
+                # Saving image
                 cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-                
-                # Crea il nome del file
                 file_name = f"{self.save_directory}/image_{self.image_count}.png"
-                
-                # Salva l'immagine
                 cv2.imwrite(file_name, cv_image)
-                
-                # Stampa un messaggio di conferma
                 rospy.loginfo(f"Saved image {file_name}")
                 
-                # Aggiorna il contatore e il tempo
+                # Updating counter and time
                 self.image_count += 1
                 self.last_saved_time = current_time
-
             except Exception as e:
                 rospy.logerr(f"Failed to save image: {e}")
 
     def run(self):
-        # Mantieni il nodo in esecuzione fino alla chiusura
+        # Executing node until the closing
         rospy.spin()
 
 if __name__ == '__main__':
     try:
-        # Crea un oggetto ImageSaver e avvialo
         image_saver = ImageSaver()
         image_saver.run()
     except rospy.ROSInterruptException:
